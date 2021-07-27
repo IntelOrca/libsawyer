13a4f1c4e3245b15b8e59cf353909570bd13083a
Ted John <ted@brambles.org>
2021-07-27 01:11:29 +0100

    Fix building without duktape and discord-rpc

--- test/ScriptingTests.cpp
+++ test/ScriptingTests.cpp
@@ -1,3 +1,5 @@
+#ifdef CS_ENABLE_SCRIPTING
+
 #include <duktape.h>
 #include <gtest/gtest.h>
 
@@ -6,3 +8,5 @@ TEST(ScriptingTests, testBasic)
     auto ctx = duk_create_heap_default();
     duk_destroy_heap(ctx);
 }
+
+#endif

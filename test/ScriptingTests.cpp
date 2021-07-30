59158d3cf381540138fb02c50cea7fd95f0c97cc
Ted John <ted@brambles.org>
2021-07-30 01:05:00 +0100

    Enable duktape C++ exceptions

--- test/ScriptingTests.cpp
+++ test/ScriptingTests.cpp
@@ -1,11 +1,60 @@
 #ifdef CS_ENABLE_SCRIPTING
 
+#include <cstdio>
 #include <duktape.h>
 #include <gtest/gtest.h>
+#include <stdexcept>
+
+#ifndef DUK_USE_CPP_EXCEPTIONS
+#error Expected C++ exceptions to be supported
+#endif
+
+static constexpr const char* CUSTOM_ERROR_MESSAGE = "A custom error occured.";
+static constexpr const char* CUSTOM_EXCEPTION_MESSAGE = "A custom exception occured.";
+
+static duk_ret_t throwError(duk_context* ctx)
+{
+    return duk_error(ctx, DUK_ERR_ERROR, CUSTOM_ERROR_MESSAGE);
+}
+
+static duk_ret_t thowException(duk_context* ctx)
+{
+    throw std::runtime_error(CUSTOM_EXCEPTION_MESSAGE);
+}
 
 TEST(ScriptingTests, testBasic)
 {
     auto ctx = duk_create_heap_default();
+    duk_eval_string(ctx, "1 + 4");
+    ASSERT_EQ(duk_get_type(ctx, -1), DUK_TYPE_NUMBER);
+    ASSERT_EQ(duk_get_int(ctx, -1), 5);
+    duk_destroy_heap(ctx);
+}
+
+TEST(ScriptingTests, testErrorHandling)
+{
+    auto ctx = duk_create_heap_default();
+    duk_push_c_function(ctx, throwError, 0);
+    duk_put_global_string(ctx, "throwError");
+    duk_push_c_function(ctx, thowException, 0);
+    duk_put_global_string(ctx, "throwException");
+
+    // Check we can catch our own errors
+    ASSERT_THROW(duk_eval_string(ctx, "throwError();"), std::runtime_error);
+
+    // Check JS can catch our own errors
+    duk_eval_string(ctx, "try { throwError(); } catch (err) { err.message; }");
+    ASSERT_EQ(duk_get_type(ctx, -1), DUK_TYPE_STRING);
+    ASSERT_STREQ(duk_get_string(ctx, -1), CUSTOM_ERROR_MESSAGE);
+
+    // Check JS can catch our own exceptions
+    duk_eval_string(ctx, "try { throwException(); } catch (err) { err.message; }");
+    ASSERT_EQ(duk_get_type(ctx, -1), DUK_TYPE_STRING);
+
+    // Check we can catch JS exceptions
+    ASSERT_THROW(duk_eval_string(ctx, "throw new Error('user exception')"), std::runtime_error);
+    ASSERT_THROW(duk_eval_string(ctx, "{}.test"), std::runtime_error);
+
     duk_destroy_heap(ctx);
 }
 

2f6d63e76eb465ec3748d554e9bceaa38283ace3
Ted John <ted@brambles.org>
2022-04-23 14:54:05 +0100

    Update CI scripts

--- go.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cmake -G Ninja -B bin -DCMAKE_BUILD_TYPE=Debug "-DCMAKE_TOOLCHAIN_FILE=M:/git/vcpkg/scripts/buildsystems/vcpkg.cmake" || goto fail
-cmake --build bin || goto fail
-cmake --install bin || goto fail
-goto :eof
-
-:fail
